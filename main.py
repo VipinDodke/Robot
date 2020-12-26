8d16bd67864ff661bc8463a90b507da87b9375be
Vipin <vipin830525@gmail.com>
2020-12-25 19:56:49 -0800

    Version 0.0.1 TWO AI created in one.

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 listener= sr.Recognizer()
 engine = pyttsx3.init()
+# engine.say("Good Evening Sir. My self Licifer, I am alway's Prasent for your help.")
 # voices = engine.getProperty('voices')
-# engine.setProperty('voice',voices[1].id)
+# engine.setProperty('voice', voices[1].id)
+# engine.say("Good Evening Sir. My self sara, I am also Prasent for your help.")
+# engine.runAndWait()
+
 def talk(text):
-    engine.say(text)
-    engine.say("Good Evening Sir. My self Licifer, I am alway's Prasent for your help.")
+    print(text)
+    if "sara" in text:
+        text=text.replace('sara','')
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[1].id)
+
+        engine.say(text)
+    elif "lucifer" in text:
+        text = text.replace('lucifer', '')
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[0].id)
+
+        engine.say(text)
     engine.runAndWait()
+def take_command():
 
-with sr.Microphone() as source:
-    listener.adjust_for_ambient_noise(source,duration=1)
-    # r.energy_threshold()
-    print("say anything : ")
+    with sr.Microphone() as source:
+        listener.adjust_for_ambient_noise(source,duration=1)
+        # r.energy_threshold()
+        print("say anything : ")
 
-    try:
-        audio = listener.listen(source)
-        commend= listener.recognize_google(audio)
+        try:
+            audio = listener.listen(source)
+            commend= listener.recognize_google(audio)
+            commend=commend.lower()
+            print(commend,"--------------?")
+            #talk(commend)
+        except:
+            pass
+        return commend
 
-        commend=commend.lower()
-        if "licifer" in commend:
-            print(commend)
-    except:
-        pass
+def run_AI():
+    command = take_command()
+    if 'play' in command:
+        command = command.replace('play', '')
+        pywhatkit.playonyt(command)
+        talk('playing '+command)
+
+run_AI()
